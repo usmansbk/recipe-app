6c27f1e128e90fd1c726476d90df69a9f3a3e637
Usman Suleiman <usmansbk@gmail.com>
2021-11-09 19:23:33 +0100

    Lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-	def index
-	end
+  def index; end
 
   protected
 
