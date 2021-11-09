051cbd801b9b68f893ddc7790ae08242c70ec328
Usman Suleiman <usmansbk@gmail.com>
2021-11-09 19:05:23 +0100

    Setup devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+	def index
+	end
+
   protected
 
   def configure_permitted_parameters
