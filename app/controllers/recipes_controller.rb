4209a35425542b29df8fc1bc2ecde72b10be9892
Usman Suleiman <usmansbk@gmail.com>
2021-11-10 19:15:25 +0100

    Style pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,7 @@ class RecipesController < ApplicationController
 
   # POST /recipes or /recipes.json
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.new(recipe_params)
 
     respond_to do |format|
       if @recipe.save
