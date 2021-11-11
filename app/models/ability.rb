1930e8c16fbcf2b0d7d565bbcf553c0f4b38ea82
Usman Suleiman <usmansbk@gmail.com>
2021-11-11 16:44:43 +0100

    Update ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,8 @@ class Ability
     return unless user.present?
 
     can :read, Recipe, public: true
-    can %i[destroy read], Recipe, user: user
-    can :destroy, Food, user: user
+    can :manage, Recipe, user: user
+    can :manage, Food, user: user
     can :read, Food
     can :create, :all
   end
