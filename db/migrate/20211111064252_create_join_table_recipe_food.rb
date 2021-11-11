ef69578c4a853c1bfc92acbdfbe0400f65a1c264
Usman Suleiman <usmansbk@gmail.com>
2021-11-11 08:45:59 +0100

    Rename join table

--- db/migrate/20211111064252_create_join_table_recipe_food.rb
+++ db/migrate/20211111064252_create_join_table_recipe_food.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableRecipeFood < ActiveRecord::Migration[6.1]
   def change
-    create_join_table :recipes, :foods, table_name: "recipe_foods" do |t|
+    create_join_table :recipes, :foods, table_name: "food_recipes" do |t|
       t.decimal :quantity
       t.index [:recipe_id, :food_id]
     end
