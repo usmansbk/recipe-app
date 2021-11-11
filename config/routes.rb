67ab955506d8778b743ca010bcde9fed1bb4e4eb
Usman Suleiman <usmansbk@gmail.com>
2021-11-11 09:42:56 +0100

    Link screen

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'foods#index'
   devise_for :users
   resources :foods, only: [:index, :create, :new, :destroy]
-  resources :recipes, only: [:index, :show, :create, :new, :destroy]
-  resources :recipe_foods, only: [:create, :update, :destroy]
+  resources :recipes, only: [:index, :show, :create, :new, :destroy] do
+    resources :recipe_foods, only: [:new, :create]
+  end
+  resources :recipe_foods, only: [:edit, :update, :destroy]
   get 'public_recipes', to: 'recipes#public_recipes'
 end
