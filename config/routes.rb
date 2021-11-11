e7030f2ffcfed01fead38c64770c02e980f91bb5
Usman Suleiman <usmansbk@gmail.com>
2021-11-11 09:08:45 +0100

    Generate views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :foods, only: [:index, :create, :new, :destroy]
   resources :recipes, only: [:index, :show, :create, :new, :destroy]
+  resources :food_recipes, only: [:create, :update, :destroy] 
   get 'public_recipes', to: 'recipes#public_recipes'
 end
