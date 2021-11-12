196658a90a4d4f15c8cf8f7db866a3e68041f389
Usman Suleiman <usmansbk@gmail.com>
2021-11-12 15:42:36 +0100

    Cascade delete

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
 
   validates :name, :measurement_unit, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods
-  has_many :foods, through: :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods, dependent: :destroy
 
   validates :name, :preparation_time, :cooking_time, :description, presence: true
 
