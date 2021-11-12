af77e59a31fef055e509daa5ccf34f061608ad89
Usman Suleiman <usmansbk@gmail.com>
2021-11-12 14:09:35 +0100

    Fix count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def show
     @recipe_foods = RecipeFood.joins(:food, :recipe).where(food: { user: current_user }, recipe: { user: current_user })
-    @items_count = @recipe_foods.count
+    @items_count = @recipe_foods.select('food_id').distinct.count
     @total_amount = @recipe_foods.sum('quantity * price')
-    @items = @recipe_foods.select('food.name, quantity, (quantity * price) as price, measurement_unit')
-      .group('food.name, quantity, price, measurement_unit')
+    @items = @recipe_foods.select('food.name, SUM(quantity) as quantity, price, measurement_unit')
+      .group('food.name, price, measurement_unit')
   end
 end
