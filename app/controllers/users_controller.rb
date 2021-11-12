4436f75acd36f4d0cb44416d3551473fc475796e
Usman Suleiman <usmansbk@gmail.com>
2021-11-12 14:12:54 +0100

    Lint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @recipe_foods = RecipeFood.joins(:food, :recipe).where(food: { user: current_user }, recipe: { user: current_user })
     @items_count = @recipe_foods.select('food_id').distinct.count
     @total_amount = @recipe_foods.sum('quantity * price')
-    @items = @recipe_foods.select('food.name, SUM(quantity) as quantity, price, measurement_unit')
-      .group('food.name, price, measurement_unit')
+    @items = @recipe_foods.group('food.name, price, measurement_unit')
+      .select('food.name, SUM(quantity) as quantity, (sum(quantity) * price) as price, measurement_unit')
   end
 end
