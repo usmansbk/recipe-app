d98877d90320d18909288b71de7fbe35ba171160
Usman Suleiman <usmansbk@gmail.com>
2021-11-12 11:47:21 +0100

    Query aggregate data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @recipe_foods = RecipeFood.joins(:food, :recipe).where(food: { user: current_user }, recipe: { user: current_user })
     @items_count = @recipe_foods.count
     @total_amount = @recipe_foods.sum('quantity * price')
-    @items = @recipe_foods.select('food.name, quantity, price, measurement_unit').group('food.name, quantity, price, measurement_unit')
+    @items = @recipe_foods.select('food.name, quantity, (quantity * price) as price, measurement_unit')
+      .group('food.name, quantity, price, measurement_unit')
   end
 end
