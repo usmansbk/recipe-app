d4c5c024ed8047eb4cb3c386693339530dfbb7a5
Usman Suleiman <usmansbk@gmail.com>
2021-11-12 11:19:18 +0100

    Query aggregate data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @foods = []
+    @recipe_foods = RecipeFood.joins(:food, :recipe).where(food: { user: current_user }, recipe: { user: current_user })
+    @items_count = @recipe_foods.count
+    @total_amount = @recipe_foods.sum('quantity * price')
   end
 end
