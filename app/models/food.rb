78adfb1b5a63b45613801b1ab7a469be36afcaa2
Usman Suleiman <usmansbk@gmail.com>
2021-11-14 09:48:38 +0100

    Add association

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+  has_many :inventory_foods, dependent: :destroy
 
   validates :name, :measurement_unit, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
