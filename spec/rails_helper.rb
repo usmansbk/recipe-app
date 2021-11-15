28d5aae56b432ec7df629679cbda42fdb49f92b0
Usman Suleiman <usmansbk@gmail.com>
2021-11-15 16:09:43 +0100

    Setup capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,8 +4,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
-require 'rspec/rails'
 require 'devise'
+require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 require_relative 'support/factory_bot'
 require_relative 'support/controller_macros'
